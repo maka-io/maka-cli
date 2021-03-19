987d6de949f2273bb5541e54e74edbcf7c821f12
maka-io <mjmcee@gmail.com>
2021-03-19 08:41:45 -0400

    version 3.2.60

--- lib/templates/layout/layout.js.jsx
+++ lib/templates/layout/layout.js.jsx
@@ -1,6 +1,16 @@
-<% if (client === 'react') { %>import React, { Component } from 'react';<% } else { %>
+import { compose } from 'ramda'; // https://ramdajs.com/docs/#compose
+<% if (client === 'react') { %>import React, { Component } from 'react';<% } else if (client === 'reflux') { %>
 import React from 'react';
 import { Component } from 'reflux';<% } %>
+// https://github.com/meteor/react-packages/tree/master/packages/react-meteor-data#withtrackerreactivefn-higher-order-component
+import { withTracker } from 'meteor/react-meteor-data';<% if(graphql === 'apollo') { %>
+import { gql } from '@apollo/client';
+// https://www.apollographql.com/docs/react/api/react/hoc/#graphqlquery-configcomponent
+import { graphql } from '@apollo/client/react/hoc';<% } %><% if (theme === 'material' ) { %>
+// https://material-ui.com/styles/basics/#higher-order-component-api
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = (theme) => { };<% } %>
 
 class <%= className %>Component extends Component {
   static propTypes = {}
@@ -18,8 +28,19 @@ class <%= className %>Component extends Component {
     super.componentWillUnmount();<% }%>
   }
 
-  render() { return (<div id="<%=fileName%>">{ this.props.children }</div>); }
+  render() {
+    return (
+      <div id="<%=fileName%>">
+        { this.props.children }
+      </div>
+    );
+  }
 }
 
-const <%= className %> = <%= className %>Component;
+const <%= className %> = compose(
+  withTracker(() => {}),<% if(graphql === 'apollo') { %>
+  graphql(gql`query Todos { todos }`, { variables: {/* input */} }),<% } %><% if(theme === 'material') { %>
+  withStyles(styles),<% } %>
+)(<%= className %>Component);
+
 export { <%= className %> };
--- lib/templates/template/template.js.jsx
+++ lib/templates/template/template.js.jsx
@@ -1,10 +1,18 @@
+import { compose } from 'ramda'; // https://ramdajs.com/docs/#compose
 <% if (client === 'react') { %>import React, { Component } from 'react';<% } else if (client === 'reflux') { %>
-import React from 'react';<% if ( isStore ) { %>
+import React from 'react';
+import PropTypes from 'prop-types';<% if ( isStore ) { %>
 import { Store } from 'reflux'; <% } else { %>
-import { Component } from 'reflux';<% } %><% } %><% if (features.withTracker !== 'false' && !isStore) { %>
-import { withTracker } from 'meteor/react-meteor-data';<% } %><% if(graphql === 'apollo' && !isStore) { %>
-import { withApollo } from 'react-apollo';<% } %>
+import { Component } from 'reflux';<% } %><% } %>
+// https://github.com/meteor/react-packages/tree/master/packages/react-meteor-data#withtrackerreactivefn-higher-order-component
+import { withTracker } from 'meteor/react-meteor-data';<% if(graphql === 'apollo') { %>
+import { gql } from '@apollo/client';
+// https://www.apollographql.com/docs/react/api/react/hoc/#graphqlquery-configcomponent
+import { graphql } from '@apollo/client/react/hoc';<% } %><% if (theme === 'material' ) { %>
+// https://material-ui.com/styles/basics/#higher-order-component-api
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = (theme) => { };<% } %>
 <% if (!isStore) { %>
 class <%= className %>Component extends Component {<% } else { %>
 class <%= className %>Component extends Store { <% } %><% if (client === 'reflux' && isStore) { %>
@@ -12,32 +20,58 @@ class <%= className %>Component extends Store { <% } %><% if (client === 'reflux
     super();
     this.state = {};
   }<% } else { %>
+  /**
+   * Define types for your props for stability.
+   */
   static propTypes = {};
 
+  /**
+   * If a prop is not required, it is a good idea to provide
+   * it a default value
+   */
   static defaultProps = {};
 
+  /**
+   * Setup the component.  Best practices are to keep this light,
+   * and offload complex behavior to methods.
+   */
   constructor(props) {
     super(props);
     this.state = {};<% if (client === 'reflux') { %>
-    this.store = null;<% } %>
+    this.stores = [];
+    this.storeKeys = [];<% } %>
   }<% } %>
 
+  /**
+   * This method runs after the component output has been rendered to the DOM.
+   * This is where you can start to interact with HTML elements, such as <div>,
+   * or attach event listeners to DOM elements.
+   */
   componentDidMount() { }
 
+  /**
+   * This is where the DOM will be torn down.  A good idea is to clean up any
+   * event listeners you've created in the componentDidMount() method.
+   */
   componentWillUnmount() {<% if (client === 'reflux' && !isStore) { %> super.componentWillUnmount();<% }%> }
 
-  render() {
+  /**
+   * This is the main render method, and will rerun anytime either this.state or this.props is changed.
+   */
+  render() {<% if (isStore) { %>
+    return null;<% } else { %>
     return (
       <div id="<% if (isComponent) { %><%= cssCaseName %>-component<% } else { %><%= cssCaseName %><% } %>">
         <h2>Find me in <%= myPath %></h2>
       </div>
-    );
+    );<% } %>
   }
-}<% if (!isStore) { %><% if (features.withTracker !== 'false') { %>
-const <%= className %> = withTracker(() => { return {}; })(<% if(graphql === 'apollo' && !isStore) { %>withApollo(<% } %><%= className %>Component<% if(graphql === 'apollo' && !isStore) { %>)<% } %>);<% } else { %>
-const <%= className %> = <%= className %>Component;<% } %>
+}
 
-export { <%= className %>, <%= className %>Component };<% } else { %>
-const <%= className %> = <%= className %>Component;
+const <%= className %> = compose(
+  withTracker(() => {}),<% if(graphql === 'apollo') { %>
+  graphql(gql`query Todos { todos }`, { variables: {/* input */} }),<% } %><% if(theme === 'material') { %>
+  withStyles(styles),<% } %>
+)(<%= className %>Component);
 
-export { <%= className %> };<% } %>
+export { <%= className %> };
